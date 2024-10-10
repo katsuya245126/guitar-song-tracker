ecf838a8c97dafa2c7400345ca813b190f71d7d8
John <katsuya245126@gmail.com>
2024-10-10 19:34:00 +0900

    Keep quote usage consistent and add search option to menu

--- JohnLeeMidtermProject.py
+++ JohnLeeMidtermProject.py
@@ -9,13 +9,15 @@ This program helps users keep track of songs they're learning. They can store
 information like song title, youtube link, tuning, and capo position.
 '''
 
+# File name to pass to functions. Should be in the same directory as the
+# main file
 file = 'guitar_songs.txt'
 
 def get_song_info():
-    title = input("Enter song title: ")
-    link = input("Enter youtube link: ")
-    tuning = input("Enter tuning: ")
-    capo = input("Enter capo position: ")
+    title = input('Enter song title: ')
+    link = input('Enter youtube link: ')
+    tuning = input('Enter tuning: ')
+    capo = input('Enter capo position: ')
 
     return [title, link, tuning, capo]
 
@@ -37,30 +39,36 @@ def print_songs(file):
             title, link, tuning, capo = song_list
 
             # Print the title on the first line
-            print(f"Title: {title}")
+            print(f'Title: {title}')
             
             # Print the rest of the details below the title with indent
-            print(f"\tYouTube Link: {link}")
-            print(f"\tTuning: {tuning}")
-            print(f"\tCapo: {capo}")
+            print(f'\tYouTube Link: {link}')
+            print(f'\tTuning: {tuning}')
+            print(f'\tCapo: {capo}')
             
             print()
 
 def print_menu():
-    print("1. Add new song")
-    print("2. Show all songs")
-    print("3. Quit")
+    print('1. Add new song')
+    print('2. Search for a song')
+    print('3. Show all songs')
+    print('4. Quit')
 
-    user_input = input("Enter your choice: ")
+    user_input = input('Enter your choice: ')
 
     match int(user_input):
         case 1:
             song_info = get_song_info()
             save_new_song(file, song_info)
         case 2:
-            print_songs(file)
+            search_song("AAA")
         case 3:
+            print_songs(file)
+        case 4:
             return
+
+def search_song(song_title):
+    print('TODO')
 
 
 print_menu()
