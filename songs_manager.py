133cc61781bbe925b663d008469f0971ff15699d
John <katsuya245126@gmail.com>
2024-12-09 03:04:21 +0900

    Add logging and add new function edit_song()

--- songs_manager.py
+++ songs_manager.py
@@ -9,13 +9,20 @@
 import json
 # Regex
 import re
+import logging
+import sys
 
 # Local module imports. Using from X import Y here to avoid having to type module name
-
-# My class
-from song import *
-# Utility functions (used in Song class too)
-from utils import get_valid_choice, pause
+try:
+    # My class
+    from song import *
+    # Utility functions
+    from utils import *
+    logging.info('Successfully imported local modules in songs_manager.py')
+except:
+    logging.critical('Failed to import utils or song in songs_manager.py')
+    print('Failed to import song or utils in songs_manager.py. Exiting...')
+    sys.exit()
 
 # Reads all the data from the file and returns as a list of dictionary of songs
 def get_all_songs(file):
@@ -24,24 +31,31 @@ def get_all_songs(file):
         # Open the file and load the content and return
         with open(file, 'r') as file:
             data = json.load(file)
+            logging.info(f'Successfully read all song data from {file.name}')
         
         return data
     except:
         # If the file is empty or we encounter
         # an error while reading file, we can just return empty list
+        logging.error(f'Something went wrong reading file: {file.name} or the list is empty.')
         return []
 
 # Prints all the songs in the given file
 def print_songs(file):
     songs_data = get_all_songs(file)
 
-    for song_dict in songs_data:
-        # values() returns the values as list, so we can unpack
-        title, link, tuning, capo = song_dict.values()
+    if not songs_data:
+        logging.warning(f'No songs found in {file}.')
+        print('\nTHE LIST IS EMPTY!\n')
+    else:
+        logging.info(f'Printing all {len(songs_data)} songs from file.')
+        for song_dict in songs_data:
+            # values() returns the values as list, so we can unpack
+            title, link, tuning, capo = song_dict.values()
 
-        # Make Song object from the data and use the class' print() function
-        # Making object for every song might seem inefficient but I don't think it matters at this scale
-        Song(title, link, tuning, capo).print()
+            # Make Song object from the data and use the class' print() function
+            # Making object for every song might seem inefficient but I don't think it matters at this scale
+            Song(title, link, tuning, capo).print()
 
 # Gets song information and returns as a list
 def get_song_info():
@@ -50,6 +64,7 @@ def get_song_info():
     new_song.set_link()
     new_song.set_tuning()
     new_song.set_capo()
+    logging.info(f'New song collected: {new_song.dict()}')
 
     return new_song  
 
@@ -67,10 +82,12 @@ def add_new_song(file):
         # Overwrites the current file
         with open(file, 'w') as file:
             json.dump(songs_list, file, indent=4)
+            logging.info(f'Added new song to {file.name}: {song_obj.dict()}')
         
         print('\nSong saved successfully!\n')
     except:
-        print('\nSomething went wrong!\n')
+        logging.error(f'Something went wrong trying to add new song to {file.name}')
+        print('\nSomething went wrong! Song not added.\n')
 
 # Searches for the given song title and prints it
 def search_song(file):
@@ -80,24 +97,53 @@ def search_song(file):
     # Clean the given song title, then make regex object to match song title. Ignores case
     song_title = song_title.strip()
     song_regex = re.compile(song_title, re.IGNORECASE)
+    logging.info(f'Searching for song: {song_title}')
 
     # Loop through all the songs 
     song_list = get_all_songs(file)
     for song_dict in song_list:
         # If song title matches the regex
         if song_regex.search(song_dict['title']):
+            logging.info(f'Matching song found: {song_dict}')
             # Make Song object and return
             title, link, tuning, capo = song_dict.values()
             song_obj = Song(title, link, tuning, capo)
 
             return song_obj
     
+    logging.info(f'No song matching {song_title} found.')
     # If no matching title is found return None
     return None
 
+# Makes changes to the song and saves it
+def edit_song(file, song_obj):
+    # Get list of all songs
+    songs_list = get_all_songs(file)
+
+    # Using enumearte here since we need the index to replace the edited song
+    for index, song in enumerate(songs_list):
+        # Compare dictionary
+        if song == song_obj.dict():
+            logging.info(f'Editing song: {song['title']}')
+            # Make changes to the song_obj and replace the matching song in the list
+            song_obj.edit()
+            songs_list[index] = song_obj.dict()
+            break
+    
+    # Save the songs list with the edited song
+    try:
+        with open(file, 'w') as file:
+            json.dump(songs_list, file, indent=4)
+            logging.info(f'Successfully saved edited song: {song_obj.dict()}')
+
+        print('\nSong updated successfully!\n')
+    except:
+        logging.error(f'Somethign went wrong trying to edit song: {song_obj.dict()}')
+        print('\nSomething went wrong! Song not edited.\n')
+
 # Saves new song information in the given file
 def delete_song(file, song_obj):
-    # Get list of all songs and append the new file
+    # Get list of all songs
     songs_list = get_all_songs(file)
 
     try:
@@ -108,10 +154,12 @@ def delete_song(file, song_obj):
         # Overwrite with the song removed
         with open(file, 'w') as file:
             json.dump(songs_list, file, indent=4)
+            logging.info(f'Successfully deleted song: {song_obj.dict()}')
         
         print("\nSong deleted!\n")
     except:
-        print("\nSomethign went wrong!\n")
+        logging.error(f'Something went wrong trying to delete song: {song_obj.dict()}')
+        print("\nSomething went wrong! Song not deleted.\n")
 
 # Manages single song options
 def manage_song(file, song_obj):
@@ -131,7 +179,7 @@ def manage_song(file, song_obj):
             case 1:
                 song_obj.open_link()
             case 2:
-                song_obj.edit()
+                edit_song(file, song_obj)
             case 3:
                 delete_song(file, song_obj)
                 # Goes back to the menu after deleting since there is nothing to edit after
