625ece28fe676648a85a410262c5e0a37c153c77
John <katsuya245126@gmail.com>
2024-12-10 01:04:16 +0900

    Combine redundant functions and add messages

--- song.py
+++ song.py
@@ -5,7 +5,7 @@
 # This class represents a song. It can print details about itself, open links, and edit attributes.
 # It can also return itself as a dictionary.
 
-# To open links
+# Module used to open links
 import webbrowser
 import logging
 import sys
@@ -21,15 +21,15 @@ except:
     sys.exit()
 
 class Song:
+    # Create new song with title, link, tuning, and capo
     def __init__(self, title="", link="", tuning="", capo=""):
-        # Create new song with title, link, tuning, and capo
         # Attributes default to empty string
         self.title = title
         self.link = link
         self.tuning = tuning
         self.capo = capo
     
-    # Prints values
+    # Prints formatted values
     def print(self):
         # Print the title on the first line
         print(f'{self.title}')
@@ -52,8 +52,14 @@ class Song:
         while True:
             title = input('Enter title: ')
         
-            # Removes spaces temporarily otherwise isalnum() returns false
+            # Have to remove spaces temporarily otherwise isalnum() returns false
             if title.replace(' ', '').isalnum():
+                # If the new title is different from current one, log it
+                # Using double quotes to surround the values in single quotes since if it's empty it might be hard to see in the log
+                if title != self.title:
+                    logging.info(f"Changing title from '{self.title}' to '{title}'")
+
+                print(f'\nChanged title from {self.title} to {title}')
                 self.title = title
                 return
             else:
@@ -67,6 +73,11 @@ class Song:
             # Link has to start with 'http://' or 'https://' otherwise the webbrowser module
             # does not open the link. Also links should not contain spaces
             if link.startswith(('http://', 'https://')) and (not ' ' in link):
+                # If the new link is different from current one, log it
+                if link != self.link:
+                    logging.info(f"Changing link from '{self.link}' to '{link}'")
+                
+                print(f'\nChanged link from {self.link} to {link}')
                 self.link = link
                 return
             else:
@@ -79,6 +90,11 @@ class Song:
         
             # Only accepts alphabetic characters.
             if tuning.isalpha():
+                # If the new tuning is different from current one, log it
+                if tuning != self.tuning:
+                    logging.info(f"Changing tuning from '{self.tuning}' to '{tuning}'")
+                
+                print(f'\nChanged tuning from {self.tuning} to {tuning}')
                 self.tuning = tuning
                 return
             else:
@@ -91,13 +107,19 @@ class Song:
         
             # Capo position should be a number and it should be between 0 (no capo) to 9.
             # Could maybe be higher but highly unlikely
+            # Have to convert to int when comparing since it's still a string
             if capo.isdigit() and (int(capo) >= 0 and int(capo) <= 9):
+                # If the new capo position is different from current one, log it
+                if capo != self.capo:
+                    logging.info(f"Changing capo position from '{self.capo}' to '{capo}'")
+                
+                print(f'\nChanged capo position from {self.capo} to {capo}')
                 self.capo = int(capo)
                 return
             else:
                 print("\nInvalid capo position. Please choose a number between 0-9.\nPlease try again\n")
     
-    # Edits the song info
+    # Edit menu handler
     def edit(self):
         while True:
             print()
@@ -113,61 +135,15 @@ class Song:
 
             match choice:
                 case 1:
-                    self.edit_title()
+                    self.set_title()
                 case 2:
-                    self.edit_link()
+                    self.set_link()
                 case 3:
-                    self.edit_tuning()
+                    self.set_tuning()
                 case 4:
-                    self.edit_capo()
+                    self.set_capo()
                 case 5:
                     break
-    
-    # Changes the title of the song
-    def edit_title(self):
-        new_title = input("Enter new title: ").strip()
-
-        # Make sure the title is not empty
-        if new_title:
-            logging.info(f'Changing title from {self.title} to {new_title}')
-            self.title = new_title
-            print("Title updated!")
-        else:
-            print("Invalid title!")
-
-    # Changes the link of the song
-    def edit_link(self):
-        new_link = input("Enter new link: ").strip()
-
-        # Make sure the link is not empty
-        if new_link:
-            logging.info(f'Changing link from {self.link} to {new_link}')
-            self.link = new_link
-            print("Link updated!")
-        else:
-            print("Invalid link!")
-
-    # Changes the tuning of the song
-    def edit_tuning(self):
-        new_tuning = input("Enter new tuning: ").strip()
-
-        # Make sure the tuning is not empty
-        if new_tuning:
-            logging.info(f'Changing tuning from {self.tuning} to {new_tuning}')
-            self.tuning = new_tuning
-            print("Tuning updated!")
-        else:
-            print("Invalid tuning!")
-
-    # Changes the capo position of the song
-    def edit_capo(self):
-        print("Choose capo position 0 - 9\n")
-
-        # Validation is handled in get_valid_choice()
-        new_capo = get_valid_choice(0, 9)
-        logging.info(f'Changing capo position from {self.capo} to {new_capo}')
-        self.capo = new_capo
-        print("Capo position updated successfully!")
         
     # Returns the object in dictionary form
     # Makes it easier to save to JSON
